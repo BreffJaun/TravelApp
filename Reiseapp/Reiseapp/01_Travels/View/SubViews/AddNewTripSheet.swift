50fc3d1dad9cc2f36a7b09c39037d776750e3a60
Jeff Braun <braun_jeff@web.de>
2025-09-11 11:26:57 +0200

    Fixed deprecated onChange behaviour.

--- Reiseapp/Reiseapp/01_Travels/View/SubViews/AddNewTripSheet.swift
+++ Reiseapp/Reiseapp/01_Travels/View/SubViews/AddNewTripSheet.swift
@@ -169,10 +169,10 @@ struct AddNewTripSheet: View {
             }
             .navigationTitle("Neue Reise")
         }
-        .onChange(of: selectedItem) { newItem in
+        .onChange(of: selectedItem) { _, newValue in
             Task {
-                guard let newItem else { return }
-                if let data = try? await newItem.loadTransferable(type: Data.self),
+                guard let newValue else { return }
+                if let data = try? await newValue.loadTransferable(type: Data.self),
                    let uiImage = UIImage(data: data) {
                     addNewTripViewModel.image = uiImage
                 }
