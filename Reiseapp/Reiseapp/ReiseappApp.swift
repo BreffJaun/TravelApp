118dc20fdcbafe3f88c186ebde2e9735f6a4ecfe
Jeff Braun <braun_jeff@web.de>
2025-09-12 14:26:02 +0200

    Minimal changes

--- Reiseapp/Reiseapp/ReiseappApp.swift
+++ Reiseapp/Reiseapp/ReiseappApp.swift
@@ -9,6 +9,19 @@ import SwiftUI
 
 @main
 struct ReiseappApp: App {
+    
+//    init() {
+//        let appearance = UINavigationBarAppearance()
+//        appearance.configureWithTransparentBackground() // transparent
+//        appearance.backgroundColor = .clear
+//        appearance.titleTextAttributes = [.foregroundColor: UIColor.label] // optional Textfarbe
+//        appearance.largeTitleTextAttributes = [.foregroundColor: UIColor.label]
+//
+//        UINavigationBar.appearance().standardAppearance = appearance
+//        UINavigationBar.appearance().scrollEdgeAppearance = appearance
+//        UINavigationBar.appearance().compactAppearance = appearance
+//    }
+    
     var body: some Scene {
         WindowGroup {
             AuthView(useLocalRepository: false)
